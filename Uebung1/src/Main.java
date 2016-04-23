4a11effe44d3d3697869521db4975c4911cd1778
HerrAugust <HerrAugust@users.noreply.github.com>
2016-04-23 20:38:18 +0200

    Update Main.java

--- Uebung1/src/Main.java
+++ Uebung1/src/Main.java
@@ -1,29 +1,37 @@
+//Uebung 1 main method
+
 import java.util.Scanner;
 
 public class Main {
     
     public static void main(String[] args) {
+    	int n = 0;
+    	System.out.println("Bitte schreiben Sie, welcher Teil Sie pruefen moechten [1,2,3,4]: ");
         Scanner c = new Scanner(System.in);
-
-        System.out.println("Bitte schreiben Sie, welcher Teil Sie prüfen möchten [1,2,3,4]: ");
-        int n = c.nextInt();
-        c.close();
+        if(c.hasNext())
+        	n = c.nextInt();
         
         switch(n) {
         case 1:
-        	Teil1.main(null);
+        	Teil1.run();
         	break;
         case 2:
-        	Teil2.main(null);
+        	Teil2.run();
         	break;
         case 3:
+        	Teil3.run();
         	break;
         case 4:
+        	Teil4.run();
         	break;
     	default:
+    		Main.main(null);
     		break;
         }
         
+        c.close();
+        
     }
 
+}
 }
