41b544df2cbfb79646bd0137b7214ed513009314
HerrAugust <HerrAugust@users.noreply.github.com>
2016-04-25 15:49:10 +0200

    Update Teil1.java

--- Uebung1/src/Teil1.java
+++ Uebung1/src/Teil1.java
@@ -1,44 +1,56 @@
+/**
+ * Schreiben Sie eine Methode, die fur eine gegebene Zahl bestimmt, ob es sich
+ * um eine Primzahl handelt (3 Punkte).
+ */
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Teil1 {
-	
-    static int anzahl = 0; //number of found prime numbers
     
-    public static void main(String[] args) {
+    public static void run() {
+    	int zahl = 0;
+    	System.out.println("Please write your max: ");
         Scanner c = new Scanner(System.in);
-
-        System.out.println("Bitte schreiben Sie seine Obergranz: ");
-        int max = c.nextInt();
+        if(c.hasNext())
+        	try {
+        		zahl = c.nextInt();
+        	}
+        catch(InputMismatchException e) {
+        	System.out.println("You must enter an integer number. Restarting...");
+        	Teil1.run();
+        	return;
+        }
         
-        methodeA(max);
+        if(zahl <= 1) {
+        	System.out.println(zahl + " must be greter than 1");
+        	return;
+        }
         
-        c.close();
+        boolean rsp = isPrime(zahl);
+        if(rsp) {
+        	System.out.println(zahl + " is prime.");
+        }
+        else {
+        	System.out.println(zahl + " is NOT prime.");
+        }
+        
     }
     
-//Teta(n)
-    private static void methodeA(int n) {
-        if(n < 0) {
-            System.out.println("You should enter a number greter than 0");
-        }
-        
-        for(int i = 1; i <= n; i++) {
-            if(isPrime(i)) {
-                anzahl++;
-                
-            }
-        }
-        System.out.println(anzahl);
-    }
-    
-//O(n)
-    public static boolean isPrime(int n) {
-        //a number is prime if you can divide it only by 1 and for the number itself
-
-        for(int i = 2; i < n; i++) {
-            if(n % i == 0)
-                return false;
-        }
-        return true;
+    /**
+     * This method determine if n is prime or not
+     * @param n the number to determine if it is prime or not
+     * @return true if n is prime, false otherwise
+     */
+    private static boolean isPrime(int n) {
+    	//A number is prime if it can be devided for 1 and itself
+    	//Test: 1,2,3,5,7,11 are prime
+    	for(int i = 2; i < n; i++) {
+    		if(n % i == 0)
+    			return false;
+    	}
+    	return true;
+    	
     }
 
 }
